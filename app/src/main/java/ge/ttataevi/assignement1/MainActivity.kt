4d097261a6d297be0190894b4e0e7765a4a39f57
tatiatt <ttata19@freeuni.edu.ge>
2023-03-30 18:03:27 +0400

    Initial commit

---
+++ app/src/main/java/ge/ttataevi/assignement1/MainActivity.kt
@@ -0,0 +1,21 @@
+package ge.ttataevi.assignement1
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.assignement)
+
+
+
+    }
+
+
+
+}
